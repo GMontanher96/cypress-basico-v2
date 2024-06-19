5f1760b5406ea7eae57697ae4e064669310ac518
Montanher <gabrielmontanherr@gmail.com>
2024-06-19 07:50:22 -0300

    Updates

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -4,3 +4,11 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    it('verifica o titulo da aplicação', function() {
+     
+    })
+})
