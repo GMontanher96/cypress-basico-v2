96866133c2790cf30d2a7416258c15215b2f638d
Montanher <gabrielmontanherr@gmail.com>
2024-06-24 22:38:05 -0300

    Updates

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -12,6 +12,46 @@ describe('Central de Atendimento ao Cliente TAT', function() {
      cy.visit('./src/index.html')
 
      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-     cy.get('#firstName').type('Olá mundo!')
+    })
+
+    it('preenche o campo nome', function() {
+        cy.get('#firstName').type('Olá mundo!')
+    })
+    it('preenche o campo sobrenome', function() {
+        cy.get('#lastName').type('montanher')
+    })
+    it('preenche o campo email', function() {
+        cy.get('#email').type('gabrielmontanherr@gmail.com')
+    })
+    it('preenche o campo telefone', function() {
+        cy.get('#phone').type('14998408445')
+    })
+    it('altera o titulo para CAT TAT', function() {
+        cy.get('#title')
+        .invoke('text', 'CAT TAT')
+    })
+    it('altera o subtitle do html', function() {
+        cy.get('#subtitle')
+        .invoke('text', 'Eu finalizei o curso de cypress com sucesso!')
+    })
+
+    it('Marca o tipo de atendimento "Feedback"', function() {
+        cy.get('input[type="radio"][value="feedback"]').check()
+    })
+    it('Marca o tipo de atendimento "Elogio"', function() {
+        cy.get('input[type="radio"][value="elogio"]').check()
+    })
+    it('Marca o tipo de atendimento "Ajuda"', function() {
+        cy.get('input[type="radio"][value="ajuda"]').check()
+    })
+
+
+    //DESAFIO ENCONTRE O GATO
+
+    it('Encontra o gato escondido', function() {
+        cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+       
     })
 })
